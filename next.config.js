589d9257c8c5b56e3ef6a18592a52754cc82d644
Sidnei Carlos da Silva Filho <001145994@MacBook-Pro-de-admrenner.local>
2022-03-18 09:29:06 -0300

    ajustado doc do challenge

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['img.lojasrenner.com.br'],
+  },
 }
 
 module.exports = nextConfig
